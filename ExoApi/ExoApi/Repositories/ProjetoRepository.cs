8b437f728437468415d4207a8cc699d3360ea584
Augusto <augustoreis2@gmail.com>
2024-09-13 20:28:45 -0300

    commit do projeto API - GET

---
+++ ExoApi/ExoApi/Repositories/ProjetoRepository.cs
@@ -0,0 +1,21 @@
+﻿using ExoApi.Domains;
+using ExoApi.Properties;
+
+namespace ExoApi.Repositories
+{
+    public class ProjetoRepository
+    {
+        private readonly ExoApiContext _context;
+
+        public ProjetoRepository( ExoApiContext context )
+        {
+            _context = context;
+        }
+
+        public List<Projeto> Listar()
+        {
+           return _context.Projetos.ToList();
+        }
+
+    }
+}
